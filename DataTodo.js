83e2b221c563c9694d910bf9af8ea465a232b846
Chanwoo-Jeong <lordroad38@naver.com>
2023-01-16 18:07:10 +0900

    Feat : data형식 및 오류 파일 정리

---
+++ DataTodo.js
@@ -0,0 +1,20 @@
+//Database 안에 Todo${index} (Todo1, Todo2) 를 추가하는 방식이다
+
+const Database = {
+  Todo1: {
+    Todo: "",
+    deadDate: "",
+    deadTime: "",
+    result: false,
+    sucessBar: 7,
+    subTodo: {
+      work1: ["줄넘기100개", false],
+      work2: ["팔굽혀펴기", false],
+      work3: ["숨쉬기운동", false],
+      work4: ["턱걸이", false],
+      work5: ["숨쉬기운동", false],
+      work6: ["프로틴먹기", false],
+      work7: ["짐볼운동하기", false],
+    },
+  },
+};
