c7a795b1489f6e412ee00515499bb539ea05cb2c
Chanwoo-Jeong <lordroad38@naver.com>
2023-01-23 23:16:07 +0900

    Feat : make Todo template

--- DataTodo.js
+++ DataTodo.js
@@ -1,20 +1,64 @@
 //Database 안에 Todo${index} (Todo1, Todo2) 를 추가하는 방식이다
 
 const Database = {
-  Todo1: {
-    Todo: "",
-    deadDate: "",
-    deadTime: "",
-    result: false,
-    sucessBar: 7,
-    subTodo: {
-      work1: ["줄넘기100개", false],
-      work2: ["팔굽혀펴기", false],
-      work3: ["숨쉬기운동", false],
-      work4: ["턱걸이", false],
-      work5: ["숨쉬기운동", false],
-      work6: ["프로틴먹기", false],
-      work7: ["짐볼운동하기", false],
-    },
-  },
+  // Todo1: {
+  //   Todo: "",
+  //   deadDate: "",
+  //   result: false,
+  //   sucessBar: 7,
+  //   subTodo: {
+  //     work1: ["줄넘기100개", false],
+  //     work2: ["팔굽혀펴기", false],
+  //     work3: ["숨쉬기운동", false],
+  //     work4: ["턱걸이", false],
+  //     work5: ["숨쉬기운동", false],
+  //     work6: ["프로틴먹기", false],
+  //     work7: ["짐볼운동하기", false],
+  //   },
+  // },
 };
+
+const TodoObj = function (todo, deadDate, result, sucessBar, subTodo) {
+  this.Todo = todo;
+  this.Date = deadDate;
+  this.Result = result;
+  this.sucessBar = sucessBar;
+  this.SubTodo = subTodo;
+};
+
+let number = 0;
+
+const tcard = document.querySelector("#temp-card");
+const div = document.createElement("div")
+const makeTodoBtn = document.querySelector("#makeTodo")
+const TodoContainerMain = document.querySelector(".TodoContainerMain")
+
+const makeTodoUI = (obj) => {
+  for (const item in obj) {
+    divo= div.cloneNode(true)
+    let html = tcard.innerHTML;
+    html = html.replace("{Todo}", Database[item]["Todo"]);
+    console.log(html)
+    divo.innerHTML = html
+    TodoContainerMain.appendChild(divo)
+  }
+};
+
+const addTodo = (todo, deadDate, result, sucessBar, subTodo) => {
+  const Obj = new TodoObj(todo, deadDate, result, sucessBar, subTodo);
+  Database[`Todo${number}`] = Obj;
+  // makeTodoUI(Database);
+  number++;
+};
+
+makeTodoBtn.addEventListener("click", (e)=>{
+  e.preventDefault()
+  addTodo(MainTodoinput.value,"밤12시까지", false, 5, { subTodo: {} })
+  // makeTodoUI(Database);
+})
+
+
+
+// addTodo("자기", "밤12시까지", false, 5, { subTodo: {} });
+// addTodo("놀기", "오전12시까지", true, 7, { subTodo: {} });
+
--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-console.log("im index")
-const todo = [
-    { work: "축구하기", deadline: "2023/01/01" },
-
-    { work: "수영하기", deadline: "2023/01/15" },
-
-    { work: "술마시기", deadline: "2023/02/01" },
-  ];
 
   let MainTodo = document.querySelector(".MainTodolist");
   let MainTodoinput = document.querySelector(".MainTodoinput");
@@ -15,10 +7,7 @@ const todo = [
   const TodoContainerSub = document.querySelector(".TodoContainerSub")
 
 
-  const todolist1 = () => {
-    console.log("i'm todolist1");
-    console.log(`해야할일 => ${todo[1].work}`);
-    // 기한 => ${todo[1].deadline}
+  const makeMainTodo = () => {
 
     const obj = document.createElement("div"); //가장 큰 틀을 생성, obj로 지정
     obj.classList.add("MainTodolist"); //"MainTodolist"라는 클래스에 추가
@@ -28,22 +17,28 @@ const todo = [
     checkBox.classList.add("chk1");
     obj.appendChild(checkBox); //체크박스를 가장 큰 틀 안에 넣어줌
 
-    const todoBox = document.createElement("div");
-    todoBox.innerHTML = MainTodoinput.value;
+    const todoBox = document.createElement("input");
+    todoBox.value = MainTodoinput.value;
+    todoBox.disabled = true
     todoBox.classList.add("MainTodoContainer");
     obj.appendChild(todoBox);
-
+    
     const editBtn = document.createElement("div"); // div class Mainicon 생성
+    editBtn.addEventListener("click",()=>{
+      console.log("나 수정버튼이에요!")
+      // todoBox.disabled = false
+      todoBox.disabled = !todoBox.disabled
+    })
     const editBtnImg = document.createElement("img") // 하위 개념인 img src 생성
     editBtnImg.src = "./edit_icon.png"; // 이미지 불러오기
-    editBtn.classList.add("Mainicon"); //css mainicon 속성 입혀주기
+    editBtn.classList.add("MainiconEdit"); //css mainicon 속성 입혀주기
     editBtn.appendChild(editBtnImg); // 버튼 이미지를 상위 editBtn div에 속하게 해주기
     obj.appendChild(editBtn); // 버튼 div 자체를 가장 큰 틀 안에 속하게 해주기
 
     const deleteBtn = document.createElement("div");
     const deleteBtnImg = document.createElement("img");
     deleteBtnImg.src = "./delete_icon.png";
-    deleteBtn.classList.add("Mainicon");
+    deleteBtn.classList.add("MainiconDlt");
     deleteBtn.appendChild(deleteBtnImg);
     obj.appendChild(deleteBtn);
 
